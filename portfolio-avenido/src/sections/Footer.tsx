7d94cf316c21470b9f32e91b05a455a5a543f847
andriusavenido <andriusavenido@gmail.com>
2025-04-06 14:57:12 -0400

    fefe

---
+++ portfolio-avenido/src/sections/Footer.tsx
@@ -0,0 +1,10 @@
+//this was created using tailwind, three js, react etc..
+const Footer:React.FC = () =>{
+    return (
+        <div className="">
+            hello
+        </div>
+    )
+}
+
+export default Footer;
